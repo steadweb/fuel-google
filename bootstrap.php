e074da50bf4d976e54d504d9244f6df4bca74b89
Luke Steadman <luke.steadman@propcom.co.uk>
2013-05-21 09:00:08 +0100

    inital commit

---
+++ bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+
+Autoloader::add_classes(array(
+	// Classes
+	'GSC_ClientLoginToken' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_OAuth2Token' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ClientError' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_TokenError' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ParseError' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_RequestError' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_Client' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_Product' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ProductList' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ManagedAccount' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ManagedAccountList' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_Datafeed' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_DatafeedList' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_User' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_UserList' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_InventoryEntry' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_InventoryEntryList' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ExampleItem' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_Issue' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_IssueGroup' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_DataQualityEntry' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_DataQualityFeed' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_ErrorElement' => __DIR__.'/classes/GShoppingContent.php',
+	'GSC_Errors' => __DIR__.'/classes/GShoppingContent.php',
+
+	 // runno
+	'_GSC_Response' => __DIR__.'/classes/GShoppingContent.php',
+	'_GSC_Http' => __DIR__.'/classes/GShoppingContent.php',
+	'_GSC_Ns' => __DIR__.'/classes/GShoppingContent.php',
+	'_GSC_Tags' => __DIR__.'/classes/GShoppingContent.php',
+	'_GSC_AtomParser' => __DIR__.'/classes/GShoppingContent.php',
+));
+
+/* End of file bootstrap.php */
